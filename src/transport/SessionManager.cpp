d7685fa0c7fc0cbdf15aae1efebb409ed7d02ce3
Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>
2026-08-31 10:49:10 +0200

    Revert "[nrf noup] Use legacy group multicast when groupcast is disabled"
    This reverts commit 9775b2c9a46464f63780d0fe7f69f8d9225c5482.

--- src/transport/SessionManager.cpp
+++ src/transport/SessionManager.cpp
@@ -225,19 +225,11 @@ CHIP_ERROR SessionManager::PrepareMessage(const SessionHandle & sessionHandle, P
             return CHIP_ERROR_INTERNAL;
         }
 
-        if (groups->IsGroupcastEnabled())
-        {
-            Credentials::GroupDataProvider::GroupInfo info;
-            ReturnErrorOnFailure(groups->GetGroupInfo(groupSession->GetFabricIndex(), groupSession->GetGroupId(), info));
-            destination_address = (info.UsePerGroupAddress())
-                ? Transport::PeerAddress::BuildMatterPerGroupMulticastAddress(fabric->GetFabricId(), groupSession->GetGroupId())
-                : Transport::PeerAddress::BuildMatterIanaMulticastAddress();
-        }
-        else
-        {
-            destination_address =
-                Transport::PeerAddress::BuildMatterPerGroupMulticastAddress(fabric->GetFabricId(), groupSession->GetGroupId());
-        }
+        Credentials::GroupDataProvider::GroupInfo info;
+        ReturnErrorOnFailure(groups->GetGroupInfo(groupSession->GetFabricIndex(), groupSession->GetGroupId(), info));
+        destination_address = (info.UsePerGroupAddress())
+            ? Transport::PeerAddress::BuildMatterPerGroupMulticastAddress(fabric->GetFabricId(), groupSession->GetGroupId())
+            : Transport::PeerAddress::BuildMatterIanaMulticastAddress();
 
         Crypto::SymmetricKeyContext * keyContext =
             groups->GetKeyContext(groupSession->GetFabricIndex(), groupSession->GetGroupId());
@@ -436,19 +428,11 @@ CHIP_ERROR SessionManager::SendPreparedMessage(const SessionHandle & sessionHand
         auto * groups = Credentials::GetGroupDataProvider();
         VerifyOrReturnError(nullptr != groups, CHIP_ERROR_INTERNAL);
 
-        if (groups->IsGroupcastEnabled())
-        {
-            Credentials::GroupDataProvider::GroupInfo info;
-            ReturnErrorOnFailure(groups->GetGroupInfo(groupSession->GetFabricIndex(), groupSession->GetGroupId(), info));
-            multicastAddress = (info.UsePerGroupAddress())
-                ? Transport::PeerAddress::BuildMatterPerGroupMulticastAddress(fabric->GetFabricId(), groupSession->GetGroupId())
-                : Transport::PeerAddress::BuildMatterIanaMulticastAddress();
-        }
-        else
-        {
-            multicastAddress =
-                Transport::PeerAddress::BuildMatterPerGroupMulticastAddress(fabric->GetFabricId(), groupSession->GetGroupId());
-        }
+        Credentials::GroupDataProvider::GroupInfo info;
+        ReturnErrorOnFailure(groups->GetGroupInfo(groupSession->GetFabricIndex(), groupSession->GetGroupId(), info));
+        multicastAddress = (info.UsePerGroupAddress())
+            ? Transport::PeerAddress::BuildMatterPerGroupMulticastAddress(fabric->GetFabricId(), groupSession->GetGroupId())
+            : Transport::PeerAddress::BuildMatterIanaMulticastAddress();
         destination      = &multicastAddress;
     }
     break;
